21130ac452a087bbfd808a5a5c2f8b965fa1d66d
Vagventure <ankitverma0366@gmail.com>
2025-09-14 13:01:00 +0530

    Established p2p connection

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,57 @@
 const username = document.getElementById("username");
 const createBtn = document.getElementById("create-user");
-const allUsers = document.getElementById("allusers");
+const allUsersHtml = document.getElementById("allusers");
+const localVideo = document.getElementById("localVideo")
+const remoteVideo = document.getElementById("remoteVideo")
+const endCallBtn = document.getElementById("end-call-btn")
+let localStream;
 const socket = io()
+let caller = [];
+
+
+const PeerConnection = (function(){
+let peerConnection;
+
+
+const createPeerConnection = ()=>{
+    const config = {
+        iceServers: [
+            {
+                urls: "stun:stun.l.google.com:19302"
+            }
+        ]
+    }
+    peerConnection = new RTCPeerConnection(config)
+
+    localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track,localStream);
+    });
+
+    peerConnection.ontrack = function(event){
+        remoteVideo.srcObject = event.streams[0];
+    }
+
+    peerConnection.onicecandidate = function(event){
+        if(event.candidate){
+            console.log(event.candidate)
+            socket.emit("icecandidate",event.candidate)
+        }
+    }
+    // console.log(peerConnection)
+    return peerConnection;
+}
+
+return{
+    getInstance: ()=>{
+        if(!peerConnection){
+            peerConnection = createPeerConnection();
+        }
+
+        return peerConnection;
+    }
+}
+})();
+
 
 createBtn.addEventListener("click", ()=>{
     const inputContainer = document.querySelector(".username-input")
@@ -16,11 +66,15 @@ createBtn.addEventListener("click", ()=>{
     `
 })
 
+endCallBtn.addEventListener("click",()=>{
+    socket.emit("call-ended", caller)
+})
+
 // handle socket events
 socket.on("joined", allusers => {
     console.log({allusers});
     const createUsersHtml = ()=>{
-        allUsers.innerHTML = "";
+        allUsersHtml.innerHTML = "";
 
         for(const user in allusers){
             const li = document.createElement("li")
@@ -41,14 +95,59 @@ socket.on("joined", allusers => {
                 li.appendChild(button);
             }
 
-            allUsers.appendChild(li);
+            allUsersHtml.appendChild(li);
         }
     }
 
     createUsersHtml();
 })
-
-
-const StartCall = (user)=>{
+socket.on("offer",async ({from,to,offer})=>{
+const pc = PeerConnection.getInstance();
+await pc.setRemoteDescription(offer);
+const answer = await pc.createAnswer();
+await pc.setLocalDescription(answer);
+socket.emit("answer", ({from,to,answer:pc.localDescription}))
+caller = [from,to]
+})
+socket.on("answer", async ({from, to, answer}) => {
+    const pc = PeerConnection.getInstance();
+    await pc.setRemoteDescription(answer);
+    // show end call button
+    endCallBtn.style.display = "block"
+    socket.emit("end-call" ,({from,to}))
+    caller = [from,to]
+});
+socket.on("icecandidate", async candidate =>{
+    const pc = PeerConnection.getInstance();
+    await pc.addIceCandidate(new RTCIceCandidate(candidate))
+})
+socket.on("end-call",({from,to})=>{
+    endCallBtn.style.display = "display";
+})
+socket.on("call-ended", caller=>{
+    EndCall();
+})
+const StartCall = async (user)=>{
     console.log(user);
+    const pc = PeerConnection.getInstance();
+    const offer = await pc.createOffer();
+    console.log({offer})
+    await pc.setLocalDescription(offer)
+    socket.emit("offer",{from:username.value, to:user, offer: pc.localDescription})
 }
+
+const EndCall = async ()=>{
+    const pc  = PeerConnection.getInstance();
+    if(pc){
+        pc.close();
+        endCallBtn.style.display = "none"
+    }
+}
+
+const StartMyCamera = async ()=>{
+    const stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true})
+    console.log({stream})
+    localStream = stream;
+    localVideo.srcObject = stream;
+}
+StartMyCamera();
--- server.js
+++ server.js
@@ -27,6 +27,26 @@ io.on("connection",(socket)=>{
         allusers[username] = {username,id:socket.id}
         io.emit("joined",allusers);
     })
+
+    socket.on("offer", ({from,to,offer}) => {
+      console.log({from,to,offer})
+      io.to(allusers[to].id).emit("offer", {from,to,offer})
+    })
+    socket.on("answer", ({from,to,answer}) => {
+      console.log({from,to,answer})
+      io.to(allusers[from].id).emit("answer", {from,to,answer})
+    })
+    socket.on("icecandidate", candidate=>{
+      socket.broadcast.emit("icecandidate", candidate)
+    })
+    socket.on("end-call", ({from,to})=>{
+      io.to(allusers[to].id).emit("end-call", ({from,to}))
+    })
+    socket.on("call-ended", caller =>{
+      const [from,to] = caller;
+      io.to(allusers[to].id).emit("call-ended", caller)
+      io.to(allusers[from].id).emit("call-ended", caller)
+    })
 })
 
 server.listen(port, () => {
