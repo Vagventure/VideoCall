9986ed467ccd2c7731c79dc51948616c83460f1b
Vagventure <ankitverma0366@gmail.com>
2025-09-16 20:32:00 +0530

    Creating room functionality and changing design

--- public/js/main.js
+++ public/js/main.js
@@ -1,90 +1,103 @@
-const username = document.getElementById("username");
 const createBtn = document.getElementById("create-user");
 const allUsersHtml = document.getElementById("allusers");
 const localVideo = document.getElementById("localVideo")
 const remoteVideo = document.getElementById("remoteVideo")
 const endCallBtn = document.getElementById("end-call-btn")
+const roomId = document.getElementById("roomid")
+const username = document.getElementById("username");
+const joinBtn = document.getElementById("join-room")
 let localStream;
+let currentRoomId;
 const socket = io()
 let caller = [];
 
 
-const PeerConnection = (function(){
-let peerConnection;
+const PeerConnection = (function () {
+    let peerConnection;
 
 
-const createPeerConnection = ()=>{
-    const config = {
-        iceServers: [
-            {
-                urls: "stun:stun.l.google.com:19302"
+    const createPeerConnection = () => {
+        const config = {
+            iceServers: [
+                {
+                    urls: "stun:stun.l.google.com:19302"
+                }
+            ]
+        }
+        peerConnection = new RTCPeerConnection(config)
+
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        });
+
+        peerConnection.ontrack = function (event) {
+            remoteVideo.srcObject = event.streams[0];
+        }
+
+        peerConnection.onicecandidate = function (event) {
+            if (event.candidate) {
+                console.log(event.candidate)
+                socket.emit("icecandidate", event.candidate)
             }
-        ]
-    }
-    peerConnection = new RTCPeerConnection(config)
-
-    localStream.getTracks().forEach(track => {
-        peerConnection.addTrack(track,localStream);
-    });
-
-    peerConnection.ontrack = function(event){
-        remoteVideo.srcObject = event.streams[0];
-    }
-
-    peerConnection.onicecandidate = function(event){
-        if(event.candidate){
-            console.log(event.candidate)
-            socket.emit("icecandidate",event.candidate)
         }
-    }
-    // console.log(peerConnection)
-    return peerConnection;
-}
-
-return{
-    getInstance: ()=>{
-        if(!peerConnection){
-            peerConnection = createPeerConnection();
-        }
-
+        // console.log(peerConnection)
         return peerConnection;
     }
-}
+
+    return {
+        getInstance: () => {
+            if (!peerConnection) {
+                peerConnection = createPeerConnection();
+            }
+
+            return peerConnection;
+        }
+    }
 })();
 
+const roomIdGenerator = () => {
+    const room = Math.floor(Math.random() * 1000000).toString();
+    currentRoomId = room;
+    roomId.innerText = `Your room id: ${room}`
+}
 
-createBtn.addEventListener("click", ()=>{
+joinBtn.addEventListener("click", () => {
     const inputContainer = document.querySelector(".username-input")
-    if(username.value != ""){
-        socket.emit("join user",username.value)
+    if (username.value != "" && roomId.value != "") {
+        socket.emit("join room", ({ username: username.value, room: roomId.value }))
     }
-    inputContainer.style.display = 'none';
+    inputContainer.classList.add("hidden");
 
-    const li = document.createElement("li");
-    li.innerText = `
-    
-    `
 })
 
-endCallBtn.addEventListener("click",()=>{
+// createBtn.addEventListener("click", ()=>{
+//     const inputContainer = document.querySelector(".username-input")
+//     if(username.value != ""){
+//         socket.emit("join user",username.value)
+//     }
+//     inputContainer.style.display = 'none';
+
+// })
+
+endCallBtn.addEventListener("click", () => {
     socket.emit("call-ended", caller)
 })
 
 // handle socket events
 socket.on("joined", allusers => {
-    console.log({allusers});
-    const createUsersHtml = ()=>{
+    console.log({ allusers });
+    const createUsersHtml = () => {
         allUsersHtml.innerHTML = "";
 
-        for(const user in allusers){
+        for (const user in allusers) {
             const li = document.createElement("li")
-            li.textContent = `${user} ${user === username.value ? "(You)":""}`
+            li.textContent = `${user} ${user === username.value ? "(You)" : ""}`
 
-            if(user!== username.value){
+            if (user !== username.value) {
                 const button = document.createElement("button")
                 button.classList.add("call-btn");
-                button.addEventListener('click', ()=>{
-                  StartCall(user);
+                button.addEventListener('click', () => {
+                    StartCall(user);
                 })
                 const img = document.createElement("img");
                 img.setAttribute("src", "/images/phone.png");
@@ -101,53 +114,84 @@ socket.on("joined", allusers => {
 
     createUsersHtml();
 })
-socket.on("offer",async ({from,to,offer})=>{
-const pc = PeerConnection.getInstance();
-await pc.setRemoteDescription(offer);
-const answer = await pc.createAnswer();
-await pc.setLocalDescription(answer);
-socket.emit("answer", ({from,to,answer:pc.localDescription}))
-caller = [from,to]
+
+socket.on("joined room", allusers => {
+    console.log({ allusers });
+    const createUsersHtml = () => {
+        allUsersHtml.innerHTML = "";
+
+        for (const room in allusers) {
+            const li = document.createElement("li")
+            li.textContent = `Room id: ${room}`
+            li.className = "room";
+            const div = document.createElement("div")
+            div.className = "room-mem"
+            for (const user in allusers[room]) {
+                const button = document.createElement("button")
+                button.style.height = "30px"
+                button.style.width = "90%";
+
+                button.innerText = `${user}`;
+                div.appendChild(button);
+                
+            }
+            li.appendChild(div);
+
+            allUsersHtml.appendChild(li);
+        }
+    }
+
+    createUsersHtml();
 })
-socket.on("answer", async ({from, to, answer}) => {
+
+socket.on("offer", async ({ from, to, offer }) => {
+    const pc = PeerConnection.getInstance();
+    await pc.setRemoteDescription(offer);
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    socket.emit("answer", ({ from, to, answer: pc.localDescription }))
+    caller = [from, to]
+})
+socket.on("answer", async ({ from, to, answer }) => {
     const pc = PeerConnection.getInstance();
     await pc.setRemoteDescription(answer);
     // show end call button
     endCallBtn.style.display = "block"
-    socket.emit("end-call" ,({from,to}))
-    caller = [from,to]
+    socket.emit("end-call", ({ from, to }))
+    caller = [from, to]
 });
-socket.on("icecandidate", async candidate =>{
+socket.on("icecandidate", async candidate => {
     const pc = PeerConnection.getInstance();
     await pc.addIceCandidate(new RTCIceCandidate(candidate))
 })
-socket.on("end-call",({from,to})=>{
+socket.on("end-call", ({ from, to }) => {
     endCallBtn.style.display = "display";
 })
-socket.on("call-ended", caller=>{
+socket.on("call-ended", caller => {
     EndCall();
 })
-const StartCall = async (user)=>{
+const StartCall = async (user) => {
     console.log(user);
     const pc = PeerConnection.getInstance();
     const offer = await pc.createOffer();
-    console.log({offer})
+    console.log({ offer })
     await pc.setLocalDescription(offer)
-    socket.emit("offer",{from:username.value, to:user, offer: pc.localDescription})
+    socket.emit("offer", { from: username.value, to: user, offer: pc.localDescription })
 }
 
-const EndCall = async ()=>{
-    const pc  = PeerConnection.getInstance();
-    if(pc){
+const EndCall = async () => {
+    const pc = PeerConnection.getInstance();
+    if (pc) {
         pc.close();
         endCallBtn.style.display = "none"
     }
 }
 
-const StartMyCamera = async ()=>{
-    const stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true})
-    console.log({stream})
+const StartMyCamera = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    console.log({ stream })
     localStream = stream;
     localVideo.srcObject = stream;
 }
 StartMyCamera();
+roomIdGenerator();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
-import {fileURLToPath} from "url"
+import { fileURLToPath } from "url"
 import { createServer } from "http"
-import { dirname,join } from "path"
+import { dirname, join } from "path"
 import { Server } from "socket.io"
 
 
@@ -16,37 +16,46 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
- console.log("Hello");
+  console.log("Hello");
   res.sendFile(join(__dirname + "/app/index.html"))
 })
 
-io.on("connection",(socket)=>{
-    console.log(`Someone has joined with the id: ${socket.id}`)
-    socket.on("join user", username =>{
-        console.log(`${username} has joined the session`)
-        allusers[username] = {username,id:socket.id}
-        io.emit("joined",allusers);
-    })
+io.on("connection", (socket) => {
+  console.log(`Someone has joined with the id: ${socket.id}`)
+  socket.on("join user", username => {
+    console.log(`${username} has joined the session`)
+    allusers[username] = { username, id: socket.id }
+    io.emit("joined", allusers);
+  })
 
-    socket.on("offer", ({from,to,offer}) => {
-      console.log({from,to,offer})
-      io.to(allusers[to].id).emit("offer", {from,to,offer})
-    })
-    socket.on("answer", ({from,to,answer}) => {
-      console.log({from,to,answer})
-      io.to(allusers[from].id).emit("answer", {from,to,answer})
-    })
-    socket.on("icecandidate", candidate=>{
-      socket.broadcast.emit("icecandidate", candidate)
-    })
-    socket.on("end-call", ({from,to})=>{
-      io.to(allusers[to].id).emit("end-call", ({from,to}))
-    })
-    socket.on("call-ended", caller =>{
-      const [from,to] = caller;
-      io.to(allusers[to].id).emit("call-ended", caller)
-      io.to(allusers[from].id).emit("call-ended", caller)
-    })
+  socket.on("join room", ({ username, room }) => {
+    console.log(`${username} has joined the room: `, room)
+    if (!allusers[room]) {
+      allusers[room] = {};
+    }
+    allusers[room][username] = { username, id: socket.id };
+    io.emit("joined room", allusers);
+  })
+
+  socket.on("offer", ({ from, to, offer }) => {
+    console.log({ from, to, offer })
+    io.to(allusers[to].id).emit("offer", { from, to, offer })
+  })
+  socket.on("answer", ({ from, to, answer }) => {
+    console.log({ from, to, answer })
+    io.to(allusers[from].id).emit("answer", { from, to, answer })
+  })
+  socket.on("icecandidate", candidate => {
+    socket.broadcast.emit("icecandidate", candidate)
+  })
+  socket.on("end-call", ({ from, to }) => {
+    io.to(allusers[to].id).emit("end-call", ({ from, to }))
+  })
+  socket.on("call-ended", caller => {
+    const [from, to] = caller;
+    io.to(allusers[to].id).emit("call-ended", caller)
+    io.to(allusers[from].id).emit("call-ended", caller)
+  })
 })
 
 server.listen(port, () => {
